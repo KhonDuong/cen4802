e89e5e48fbf25f9372de5c618c11eb7723fc16d9
Khon <duongdilan@yahoo.com>
2023-05-21 20:06:12 -0400

    Update with JavaDocs

--- assignment2/src/Main.java
+++ assignment2/src/Main.java
@@ -4,6 +4,13 @@ public class Main {
         System.out.println("The value of term " + n + " in the Fibonacci sequence is " + fibSeq(10));
     }
 
+    /**
+     * Returns a value of type long of the Fibonacci sequence given the param nThTerm
+     * The function for the Fibonacci sequence is represented as Fn = F(n-1) + F(n-2)
+     *
+     * @param nthTerm   the nth term in the Fibonacci sequence desired
+     * @return          the result after finding the nth term in the Fibonacci sequence
+     */
     public static long fibSeq(int nthTerm)
     {
         // For terms 0 and 1, you can't do fn = fn-1 + fn-2.
